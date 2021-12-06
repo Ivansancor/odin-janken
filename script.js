277677e76684cff99e6efa2a4fcd74626dbe5272
Ivansancor <ivanvaldeolivo95@gmail.com>
2021-12-06 21:46:06 +0100

    added some basic js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello bitches!");
+let computerGuess = "Rock";
+let compGuessProcess = 0;
+function computerPlay(){
+    compGuessProcess = Math.random()*100 + 1;
+    if(compGuessProcess >= 0 && compGuessProcess < 33){
+        return computerGuess = "Rock";
+    }
+    else if(compGuessProcess >= 33 && compGuessProcess <= 66){
+        return computerGuess = "Paper";
+    } else {return "Scissors";}
+
+}
+computerPlay();
+console.log(computerGuess);
+
+let userGuess = "rock";
+
+function playRound(computerGuess, userGuess){
+    if(userGuess === "rock" && computerGuess === "Rock" || userGuess === "/paper/i" && computerGuess === "Paper" || userGuess === "/scissors/i" && computerGuess === "Scissors"){
+        return "It's a tie!"
+    } else {return "One of you wins and the other loses!";}
+}
+console.log(userGuess);
+console.log(playRound(computerGuess, userGuess));
