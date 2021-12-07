e8055f47697bbfc887025cf2df10dbf6443d47f6
Ivansancor <ivanvaldeolivo95@gmail.com>
2021-12-07 14:57:08 +0100

    making changes to js to prompt player

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-let computerGuess = "Rock";
+let computerGuess = "";
 let compGuessProcess = 0;
 function computerPlay(){
-    compGuessProcess = Math.random()*100 + 1;
-    if(compGuessProcess >= 0 && compGuessProcess < 33){
+    compGuessProcess = Math.floor(Math.random()*100) + 1;
+    console.log(compGuessProcess);
+    if(compGuessProcess >= 1 && compGuessProcess < 33){
         return computerGuess = "Rock";
     }
     else if(compGuessProcess >= 33 && compGuessProcess <= 66){
         return computerGuess = "Paper";
-    } else {return "Scissors";}
+    } else {return computerGuess = "Scissors";}
 
 }
 computerPlay();
 console.log(computerGuess);
 
-let userGuess = "rock";
+let userGuess = prompt("Enter your guess");
+console.log(userGuess);
 
 function playRound(computerGuess, userGuess){
-    if(userGuess === "rock" && computerGuess === "Rock" || userGuess === "/paper/i" && computerGuess === "Paper" || userGuess === "/scissors/i" && computerGuess === "Scissors"){
-        return "It's a tie!"
+    if(userGuess === "rock" && computerGuess === "Rock" || userGuess === "paper" && computerGuess === "Paper" || userGuess === "scissors" && computerGuess === "Scissors"){
+        return "It's a tie!";
     } else {return "One of you wins and the other loses!";}
 }
-console.log(userGuess);
+
 console.log(playRound(computerGuess, userGuess));
