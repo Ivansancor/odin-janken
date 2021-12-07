02537cb6e29ced99ee3f538e5c5cba0f5c224ad9
Ivansancor <ivanvaldeolivo95@gmail.com>
2021-12-07 15:43:16 +0100

    fixed regex problems and finished basic functionality for one round

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let computerGuess = "";
 let compGuessProcess = 0;
 function computerPlay(){
     compGuessProcess = Math.floor(Math.random()*100) + 1;
-    //console.log(compGuessProcess);
+    // console.log(compGuessProcess);
     if(compGuessProcess >= 1 && compGuessProcess < 33){
         return computerGuess = "Rock";
     }
@@ -14,8 +14,21 @@ function computerPlay(){
 computerPlay();
 // console.log(computerGuess);
 
-let userGuess = prompt("Enter your guess");
-//console.log(userGuess);
+let preuserGuess = prompt("Enter your guess");
+let userGuess = "";
+let regexOne = /^rock$/i;
+let regexTwo = /^paper$/i;
+let regexThree = /^scissors$/i;
+
+function userGuessValidity(){
+    if(preuserGuess.match(regexOne) || preuserGuess.match(regexTwo) || preuserGuess.match(regexThree)){
+        return userGuess = preuserGuess.toLowerCase();
+    } else {alert("Invalid choice!");
+    preuserGuess = prompt("Enter a valid guess");
+    userGuessValidity();
+    }
+}
+
 
 function playRound(computerGuess, userGuess){
     if(userGuess === "rock" && computerGuess === "Rock" || userGuess === "paper" && computerGuess === "Paper" || userGuess === "scissors" && computerGuess === "Scissors"){
@@ -23,7 +36,8 @@ function playRound(computerGuess, userGuess){
     }
     else if(userGuess === "rock" && computerGuess === "Paper" || userGuess === "scissors" && computerGuess === "Rock" || userGuess === "paper" && computerGuess === "Scissors"){
         return "I win!";
-    } else { return "YOU WIN!!!";}
+    } 
+    else { return "YOU WIN!!!";}
 }
-
+userGuessValidity();
 console.log(playRound(computerGuess, userGuess));
